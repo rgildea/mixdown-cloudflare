778e12afb17f35f41d85c6e1d2b02e8e0524c670
Ryan Gildea <ryangildea@gmail.com>
2024-03-20 19:45:08 -0400

    fix bad import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { marketingPreset } from '#app/routes/_marketing+/tailwind-preset.ts'
+import { marketingPreset } from './app/routes/_marketing+/tailwind-preset.ts'
 import { type Config } from 'tailwindcss'
 import animatePlugin from 'tailwindcss-animate'
 import radixPlugin from 'tailwindcss-radix'
