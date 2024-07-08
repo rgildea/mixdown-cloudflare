401943246668577a3b650a83c14bedc679655036
Ryan Gildea <ryangildea@gmail.com>
2024-07-08 10:19:48 -0400

    switched to Neon DB

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,21 +1,37 @@
-import { PrismaClient as PrismaEdgeClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { PrismaClient as PrismaEdgeClient } from '@prisma/client/edge'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
 import chalk from 'chalk'
+import ws from 'ws'
 
-export const prisma = (databaseUrl: string) => {
+export const prisma = (connectionString: string) => {
 	// NOTE: if you change anything in this function you'll need to restart
 	// the dev server to see your changes.
 
 	// Feel free to change this log threshold to something that makes sense for you
 	const logThreshold = 500 // ms
-	const client = new PrismaEdgeClient({
-		datasources: { db: { url: databaseUrl } },
+
+	neonConfig.webSocketConstructor = ws
+	const pool = new Pool({ connectionString })
+	const adapter = new PrismaNeon(pool)
+	const client = new PrismaClient({
+		adapter,
 		log: [
 			{ level: 'query', emit: 'event' },
 			{ level: 'error', emit: 'stdout' },
 			{ level: 'warn', emit: 'stdout' },
 		],
 	})
+
+	// const client = new PrismaClient({
+	// 	datasources: { db: { url: databaseUrl } },
+	// 	log: [
+	// 		{ level: 'query', emit: 'event' },
+	// 		{ level: 'error', emit: 'stdout' },
+	// 		{ level: 'warn', emit: 'stdout' },
+	// 	],
+	// })
 	client.$on('query', async e => {
 		if (e.duration < logThreshold) return
 		const color =
@@ -31,7 +47,6 @@ export const prisma = (databaseUrl: string) => {
 		const dur = chalk[color](`${e.duration}ms`)
 		console.info(`prisma:query - ${dur} - ${e.query}`)
 	})
-	client.$extends(withAccelerate())
 	client.$connect()
 	return client
 }
