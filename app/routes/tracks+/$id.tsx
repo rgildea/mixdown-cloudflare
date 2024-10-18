cc049ad2332a9ee9779e39e600a7b9fc535e6977
Ryan Gildea <ryangildea@gmail.com>
2024-10-18 13:29:13 -0400

    fix: do not make Responses in the global scope

--- app/routes/tracks+/$id.tsx
+++ app/routes/tracks+/$id.tsx
@@ -13,8 +13,9 @@ import { Link, Outlet, useActionData, useFetcher, useLoaderData } from '@remix-r
 import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
 import { z } from 'zod'
-const notFoundResponse = new Response('Not found', { status: 404 })
+
 export type TrackFormAction = 'set-active-version' | 'edit-title'
+
 const EditTitleSchema = z.object({ intent: z.literal('edit-title'), title: z.string().min(5).max(80) })
 const SetActiveVersionSchema = z.object({
 	intent: z.literal('set-active-version'),
@@ -105,6 +106,7 @@ export const action = async ({ request, params, context: { storageContext } }: A
 
 export const loader = async ({ params, context }: LoaderFunctionArgs) => {
 	const trackId = params.id as string
+	const notFoundResponse = new Response('Not found', { status: 404 })
 
 	const track = await getTrackWithVersionsByTrackId(context.storageContext, trackId)
 
@@ -118,7 +120,6 @@ export const loader = async ({ params, context }: LoaderFunctionArgs) => {
 
 const TrackRoute: React.FC = () => {
 	const { track } = useLoaderData<typeof loader>() // get the track from the loader data
-
 	const versions = track.trackVersions
 	const actionData = useActionData<typeof action>() // get the last action result
 	const playerDispatch = usePlayerDispatchContext() // get the player dispatch function
